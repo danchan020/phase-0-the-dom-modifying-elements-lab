1d083072a47f2b9fb92e7d401241d3661673d6c4
Daniel <danchan020@gmail.com>
2022-05-02 08:57:37 -0700

    commit tree 71510760f94b167a46f8e31ee58aa0edceb6e98a

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.querySelector("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "YOUR-NAME is the champion"
